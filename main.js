6d0ab5b4a1162e110e52d489c4638ca88ab74260
chih-hsiang <chuangchihhsiang@gmail.com>
2016-09-04 12:03:05 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ towerImg.src = "images/tower.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 setTimeout(draw,1000);
+var tower = {
+  x:0;
+  y:0;
+}
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(towerImg,0,0);
