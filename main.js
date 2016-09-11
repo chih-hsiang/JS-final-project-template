8baef567ac9f83f5e01a45df429d1b8d532de9bf
chih-hsiang <chuangchihhsiang@gmail.com>
2016-09-11 10:55:18 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var towerImg = document.createElement("img");
 towerImg.src = "images/tower.png";
+var towerImg = document.createElement("img");
+towerImg.src = "images/tower.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 setInterval(draw,16);
