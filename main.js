2ec6082ff4524379dde6f08c9a20cda00726f9b4
chih-hsiang <chuangchihhsiang@gmail.com>
2016-09-11 10:58:26 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ var bgImg = document.createElement("img");
 bgImg.src = "images/map.png";
 var towerImg = document.createElement("img");
 towerImg.src = "images/tower.png";
-var towerImg = document.createElement("img");
-towerImg.src = "images/tower.png";
+var tower-btnImg = document.createElement("img");
+tower-btnImg.src = "images/tower-btn.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 setInterval(draw,16);
@@ -14,4 +14,5 @@ var tower = {
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(towerImg,tower.x,tower.y);
+  ctx.drawImage(tower-btnImg,608,448,32,32);
 }
