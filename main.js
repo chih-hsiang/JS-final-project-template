9f51dc182eac6308cbc3583c99d78f6b5ad5d52c
chih-hsiang <chuangchihhsiang@gmail.com>
2016-09-11 12:23:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,18 @@ towerbtnImg.src = "images/tower-btn.png";
 var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 setInterval(draw,16);
+var isBuilding = false;
 var tower = {
   x:0,
   y:0
 };
-$("#game-canvas").on("mousemove",function(event){
+$("#game-canvas").on("mousemove",function(){
   tower.x = event.offsetX;
   tower.y = event.offsetY;
+});
+$("#game-canvas").on("click",function(event){
+  if(tower.x >= 640-64 &&tower.y >= 480-64){
+    console.log("aaaa");
 });
 function draw(){
   ctx.drawImage(bgImg,0,0);
