6221c5c121c63023e52a9f456e03a1140a96d02d
chih-hsiang <chuangchihhsiang@gmail.com>
2016-09-11 12:24:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ $("#game-canvas").on("mousemove",function(){
 $("#game-canvas").on("click",function(event){
   if(tower.x >= 640-64 &&tower.y >= 480-64){
     console.log("aaaa");
-});
+}});
 function draw(){
   ctx.drawImage(bgImg,0,0);
   ctx.drawImage(towerImg,tower.x,tower.y);
