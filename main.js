92dbcac85f547ac540eb9b7132f464afa3f48be4
chih-hsiang <chuangchihhsiang@gmail.com>
2016-10-02 12:19:09 +0800

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ var enemy = {
   speedx:0,
   speedy:-64,
   move:function(){
-    if(isCollided(path[this.pathDes].x,path[this.pathDes].y,this.x,this.y,this.speedx/FPS,this.speedy/FPS);){
+    if(true){
       console.log("aaaaa");
       this.x = path[this.pathDes].x;
       this.y = path[this.pathDes].y;
