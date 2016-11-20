ac62248365bb78456d6af9bc25dbb51b8df6e1c4
chih-hsiang <chuangchihhsiang@gmail.com>
2016-11-20 10:24:57 +0800

    Update main.js

--- main.js
+++ main.js
@@ -150,6 +150,9 @@ function cursor(){
 $("#game-canvas").on("mousemove",function(){
   tower.x = event.offsetX - event.offsetX%32;
   tower.y = event.offsetY - event.offsetY%32;
+  if(isBuiding == false){
+    
+  }
 });
 $("#game-canvas").on("click",function(event){
   if(tower.x >= 640-64 &&tower.y >= 480-64){
